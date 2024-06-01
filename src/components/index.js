ad1e96237676c4e1cc266589b7da69693c6930f5
rohit-ku-patel <rp58007@gmail.com>
2024-06-01 21:10:11 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./header/Header";
+import Select from './Select'
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import LogOutBtn from "./header/LogOutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Button from './Button'
+import Login from './Login'
+import Logo from "./Logo";
+import Input from "./Input";
+import PostForm from "./post-form/PostForm";
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogOutBtn,
+  RTE,
+  Signup,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout, 
+  Input,
+  Select,
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from '../components/index'
+const Login = () => {
+  return (
+    <div py-8>
+      <Login />
+    </div>
+  )
+}
+
+export default Login
